1c4b899ce2ee0bbc642e03e238baec01e65a30f4
jochen <jochen@byte23.net>
2024-10-23 14:44:49 +0200

    basic-router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from 'react-router-dom'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="pokemon" element={<Pokemon />} />
+        </Route>
+    )
+)
+
 function App() {
     return (
         <>
