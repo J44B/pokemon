6aec8104b4cfd35fbe3bc4492c4c08fa96acb508
jochen <jochen@byte23.net>
2024-10-23 15:30:00 +0200

    PokemonCard and Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import PokemonCard from '../components/PokemonCard'
+
 function Home() {
-    return <h1 className="text-3xl underline mt-4">Hello Pokemon!</h1>
+    return (
+        <div>
+            <h1 className="text-3xl underline mt-4">Hello Pokemon!</h1>
+            <PokemonCard />
+        </div>
+    )
 }
 
 export default Home
